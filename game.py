c59a4b4c9b7a72f254f23db0e75c639deeae59ec
Turzhanov <umitkhanturzhanov@gmail.com>
2022-06-22 22:22:57 +0600

    Update game.py

--- game.py
+++ game.py
@@ -18,7 +18,37 @@ canvas = Canvas(window, WIDTH, HEIGHT)
 
 menu_buttons = []
 background = NewImg(canvas, "images/background.png", 0, 0)
+from window import Window
 
+window = Window("События", 300, 300)
+
+def right(event):
+
+    print("Клавиша стрелка вправо нажата")
+
+    print(event)
+
+    print(f"Координата X {event.x}, Координата Y {event.y}")
+
+
+
+window.bind("<Right>", right)
+
+
+
+def motion(event):
+
+    print("Перемещение мыши")
+
+    print(event)
+
+    print(f"Координата X {event.x}, Координата Y {event.y}")
+
+
+
+window.bind("<Motion>", motion)
+
+window.mainloop() 
 
 
 window.mainloop()
